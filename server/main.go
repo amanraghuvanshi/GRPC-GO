afa6d2559cc2fa8cf66cd1915f8ea0e81bfe4bd8
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-02-18 12:02:37 +0530

    updates

--- server/main.go
+++ server/main.go
@@ -1,1 +1,26 @@
-package server
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	// # starting the server at on the machine
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	// starting the gRPC server on the machines
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start the gRPC Server, %v", err)
+	}
+}
