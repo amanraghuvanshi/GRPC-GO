d6b375a4da2eb16d07153d2ecb2248b6ad5ae328
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-02-18 12:14:32 +0530

    minor

--- client/main.go
+++ client/main.go
@@ -1,5 +1,26 @@
 package client
 
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
 func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Unable to connect %v", err)
+	}
+
+	// ensuring runs at the very end
+	defer conn.Close()
+
+	// client := pb.NewGreetServiceClient(conn)
 
 }
